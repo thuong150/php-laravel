6176264f0e718027698667c0a3661751e154c26d
thuong150 <99636449+thuong150@users.noreply.github.com>
2023-03-16 00:31:48 +0700

    database
    database

---
+++ database.sql
@@ -0,0 +1,127 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               8.0.30 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.1.0.6537
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Dumping data for table ocean.blogs: ~6 rows (approximately)
+INSERT INTO `blogs` (`id`, `user_id`, `title`, `subtitle`, `image`, `content`, `created_at`, `updated_at`) VALUES
+	(1, 1, 'Đặc tính của nước hoa hàng hiệu cần phải biết !', 'Nước hoa là một trong những sản phẩm làm đẹp không thể thiếu của phái đẹp. Nó không chỉ mang lại hương thơm quyến rũ mà còn tôn lên sự tự tin, phong cách và cá tính của người sử dụng.', 'blog-1.png', 'Nước hoa là một trong những sản phẩm làm đẹp không thể thiếu của phái đẹp. Nó không chỉ mang lại hương thơm quyến rũ mà còn tôn lên sự tự tin, phong cách và cá tính của người sử dụng.Nước hoa là một trong những sản phẩm làm đẹp không thể thiếu của phái đẹp. Nó không chỉ mang lại hương thơm quyến rũ mà còn tôn lên sự tự tin, phong cách và cá tính của người sử dụng.Nước hoa là một trong những sản phẩm làm đẹp không thể thiếu của phái đẹp. Nó không chỉ mang lại hương thơm quyến rũ mà còn tôn lên sự tự tin, phong cách và cá tính của người sử dụng.Nước hoa là một trong những sản phẩm làm đẹp không thể thiếu của phái đẹp. Nó không chỉ mang lại hương thơm quyến rũ mà còn tôn lên sự tự tin, phong cách và cá tính của người sử dụng.Nước hoa là một trong những sản phẩm làm đẹp không thể thiếu của phái đẹp. Nó không chỉ mang lại hương thơm quyến rũ mà còn tôn lên sự tự tin, phong cách và cá tính của người sử dụng.Nước hoa là một trong những sản phẩm làm đẹp không thể thiếu của phái đẹp. Nó không chỉ mang lại hương thơm quyến rũ mà còn tôn lên sự tự tin, phong cách và cá tính của người sử dụng.Nước hoa là một trong những sản phẩm làm đẹp không thể thiếu của phái đẹp. Nó không chỉ mang lại hương thơm quyến rũ mà còn tôn lên sự tự tin, phong cách và cá tính của người sử dụng.', '2023-03-08 19:16:44', '2023-03-08 19:16:47'),
+	(2, 1, '12 chai nước hoa nổi tiếng nhất thế giới', 'Nước hoa là một trong những sản phẩm làm đẹp không thể thiếu của phái đẹp. Nó không chỉ mang lại hương thơm quyến rũ mà còn tôn lên sự tự tin, phong cách và cá tính của người sử dụng.', 'blog-2.png', 'Nước hoa là một trong những sản phẩm làm đẹp không thể thiếu của phái đẹp. Nó không chỉ mang lại hương thơm quyến rũ mà còn tôn lên sự tự tin, phong cách và cá tính của người sử dụng.', '2023-03-08 19:16:45', '2023-03-08 19:16:48'),
+	(3, 2, 'Mẹo giữ nước hoa thơm lâu', 'Nước hoa là một trong những sản phẩm làm đẹp không thể thiếu của phái đẹp. Nó không chỉ mang lại hương thơm quyến rũ mà còn tôn lên sự tự tin, phong cách và cá tính của người sử dụng.', 'blog-4.png', 'Nước hoa là một trong những sản phẩm làm đẹp không thể thiếu của phái đẹp. Nó không chỉ mang lại hương thơm quyến rũ mà còn tôn lên sự tự tin, phong cách và cá tính của người sử dụng.\r\n\r\nĐể chọn được một chai nước hoa phù hợp, bạn cần phải biết rõ về mùi hương, độ lưu hương và thời điểm sử dụng. Có nhiều loại nước hoa khác nhau, từ các loại nước hoa thơm nhẹ cho đến các loại nước hoa đậm đặc hơn.\r\n\r\nThời gian lưu hương của mỗi loại nước hoa cũng khác nhau, từ vài giờ đến một ngày. Vì vậy, bạn cần phải lựa chọn loại nước hoa phù hợp với hoạt động và thời gian sử dụng của mình.\r\n\r\nKhông chỉ đơn thuần là sản phẩm dành cho phái đẹp, nước hoa cũng là một món quà tuyệt vời dành cho mọi người. Bạn có thể tặng nước hoa cho người thân, bạn bè hoặc người yêu vào các dịp đặc biệt như sinh nhật, kỷ niệm hay Giáng sinh.\r\n\r\nTuy nhiên, bạn cần chú ý khi tặng nước hoa vì mỗi người có sở thích và phong cách khác nhau. Hãy tìm hiểu về thương hiệu và mùi hương yêu thích của người nhận để chọn được sản phẩm phù hợp nhất.\r\n\r\nVới những lợi ích vượt trội như vậy, nước hoa đang trở thành sản phẩm không thể thiếu trong cuộc sống của mọi người. Nó không chỉ mang lại hương thơm quyến rũ mà còn là biểu tượng của sự thời thượng và tinh tế trong phong cách sống.', '2023-03-08 19:16:45', '2023-03-08 19:16:48'),
+	(4, 3, '7 loại nước hoa nam kinh điển khiến các nàng mê như điếu đổ', 'Nước hoa là một trong những sản phẩm làm đẹp không thể thiếu của phái đẹp. Nó không chỉ mang lại hương thơm quyến rũ mà còn tôn lên sự tự tin, phong cách và cá tính của người sử dụng.', 'blog-6.png', 'Nước hoa là một trong những sản phẩm làm đẹp không thể thiếu của phái đẹp. Nó không chỉ mang lại hương thơm quyến rũ mà còn tôn lên sự tự tin, phong cách và cá tính của người sử dụng.', '2023-03-08 19:16:46', '2023-03-08 19:16:49'),
+	(5, 3, '7 loại nước hoa nam kinh điển khiến các nàng mê như điếu đổ', 'Nước hoa là một trong những sản phẩm làm đẹp không thể thiếu của phái đẹp. Nó không chỉ mang lại hương thơm quyến rũ mà còn tôn lên sự tự tin, phong cách và cá tính của người sử dụng.', 'blog-6.png', 'Nước hoa là một trong những sản phẩm làm đẹp không thể thiếu của phái đẹp. Nó không chỉ mang lại hương thơm quyến rũ mà còn tôn lên sự tự tin, phong cách và cá tính của người sử dụng.', '2023-03-08 19:16:46', '2023-03-08 19:16:49'),
+	(6, 2, 'Mẹo giữ nước hoa thơm lâu', 'Nước hoa là một trong những sản phẩm làm đẹp không thể thiếu của phái đẹp. Nó không chỉ mang lại hương thơm quyến rũ mà còn tôn lên sự tự tin, phong cách và cá tính của người sử dụng.', 'blog-4.png', 'Nước hoa là một trong những sản phẩm làm đẹp không thể thiếu của phái đẹp. Nó không chỉ mang lại hương thơm quyến rũ mà còn tôn lên sự tự tin, phong cách và cá tính của người sử dụng.', '2023-03-08 19:16:45', '2023-03-08 19:16:48');
+
+-- Dumping data for table ocean.brands: ~4 rows (approximately)
+INSERT INTO `brands` (`id`, `name`, `created_at`, `updated_at`) VALUES
+	(1, 'chanel', '2023-03-08 19:17:56', '2023-03-08 19:17:59'),
+	(2, 'lancome', '2023-03-08 19:17:56', '2023-03-08 19:18:00'),
+	(3, 'gucci', '2023-03-08 19:17:57', '2023-03-08 19:18:01'),
+	(4, 'dg', '2023-03-08 19:17:57', '2023-03-08 19:18:01'),
+	(5, 'dior', '2023-03-08 19:17:58', '2023-03-08 19:18:02');
+
+-- Dumping data for table ocean.failed_jobs: ~0 rows (approximately)
+
+-- Dumping data for table ocean.migrations: ~0 rows (approximately)
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+	(1, '2014_10_12_000000_create_users_table', 1),
+	(2, '2014_10_12_100000_create_password_resets_table', 1),
+	(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+	(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+	(5, '2023_03_08_145329_create_orders_table', 1),
+	(6, '2023_03_08_150844_create_brands_table', 1),
+	(7, '2023_03_08_150933_create_product_cats_table', 1),
+	(8, '2023_03_08_151211_create_blogs_table', 1),
+	(9, '2023_03_08_151503_create_order_details_table', 1),
+	(10, '2023_03_08_151710_create_products_table', 1),
+	(11, '2023_03_08_151737_create_product_comments_table', 1),
+	(12, '2023_03_08_151755_create_product_details_table', 1),
+	(13, '2023_03_08_151821_create_product_images_table', 1),
+	(14, '2023_03_12_070603_create_volumes_table', 2);
+
+-- Dumping data for table ocean.orders: ~0 rows (approximately)
+
+-- Dumping data for table ocean.order_details: ~0 rows (approximately)
+
+-- Dumping data for table ocean.password_resets: ~0 rows (approximately)
+
+-- Dumping data for table ocean.personal_access_tokens: ~0 rows (approximately)
+
+-- Dumping data for table ocean.products: ~9 rows (approximately)
+INSERT INTO `products` (`id`, `brand_id`, `product_category_id`, `name`, `description`, `content`, `price`, `qty`, `discount`, `capacity`, `featured`, `created_at`, `updated_at`) VALUES
+	(1, 1, 1, 'Sản phẩm 1', 'Đây là một trong những nước hoa nổi tiếng nhất của Chanel với mùi hương hoa cỏ pha trộn cùng hương thơm đậm đà của vani và hổ phách.', 'Đây là một trong những nước hoa nổi tiếng nhất của Chanel với mùi hương hoa cỏ pha trộn cùng hương thơm đậm đà của vani và hổ phách.', 1000000, 18, 1, 10, 1, '2023-03-08 19:25:43', '2023-03-08 19:25:46'),
+	(2, 2, 1, 'Sản phẩm 2', 'Đây là một trong những nước hoa nổi tiếng nhất của Chanel với mùi hương hoa cỏ pha trộn cùng hương thơm đậm đà của vani và hổ phách.', 'Đây là một trong những nước hoa nổi tiếng nhất của Chanel với mùi hương hoa cỏ pha trộn cùng hương thơm đậm đà của vani và hổ phách.', 2000000, 18, 1, 10, 1, '2023-03-08 19:25:43', '2023-03-08 19:25:46'),
+	(3, 3, 1, 'Sản phẩm 3', 'Đây là một trong những nước hoa nổi tiếng nhất của Chanel với mùi hương hoa cỏ pha trộn cùng hương thơm đậm đà của vani và hổ phách.', 'Đây là một trong những nước hoa nổi tiếng nhất của Chanel với mùi hương hoa cỏ pha trộn cùng hương thơm đậm đà của vani và hổ phách.', 3000000, 18, 1, 10, 1, '2023-03-08 19:25:43', '2023-03-08 19:25:46'),
+	(4, 4, 1, 'Sản phẩm 4', 'Đây là một trong những nước hoa nổi tiếng nhất của Chanel với mùi hương hoa cỏ pha trộn cùng hương thơm đậm đà của vani và hổ phách.', 'Đây là một trong những nước hoa nổi tiếng nhất của Chanel với mùi hương hoa cỏ pha trộn cùng hương thơm đậm đà của vani và hổ phách.', 4000000, 18, 1, 10, 1, '2023-03-08 19:25:43', '2023-03-08 19:25:46'),
+	(5, 5, 1, 'Sản phẩm 5', 'Đây là một trong những nước hoa nổi tiếng nhất của Chanel với mùi hương hoa cỏ pha trộn cùng hương thơm đậm đà của vani và hổ phách.', 'Đây là một trong những nước hoa nổi tiếng nhất của Chanel với mùi hương hoa cỏ pha trộn cùng hương thơm đậm đà của vani và hổ phách.', 5000000, 18, 1, 10, 1, '2023-03-08 19:25:43', '2023-03-08 19:25:46'),
+	(6, 4, 2, 'Sản phẩm 6', 'Đây là một trong những nước hoa nổi tiếng nhất của Chanel với mùi hương hoa cỏ pha trộn cùng hương thơm đậm đà của vani và hổ phách.', 'Đây là một trong những nước hoa nổi tiếng nhất của Chanel với mùi hương hoa cỏ pha trộn cùng hương thơm đậm đà của vani và hổ phách.', 6000000, 18, 1, 10, 1, '2023-03-08 19:25:43', '2023-03-08 19:25:46'),
+	(7, 3, 2, 'Sản phẩm 7', 'Đây là một trong những nước hoa nổi tiếng nhất của Chanel với mùi hương hoa cỏ pha trộn cùng hương thơm đậm đà của vani và hổ phách.', 'Đây là một trong những nước hoa nổi tiếng nhất của Chanel với mùi hương hoa cỏ pha trộn cùng hương thơm đậm đà của vani và hổ phách.', 7000000, 18, 1, 10, 1, '2023-03-08 19:25:43', '2023-03-08 19:25:46'),
+	(8, 2, 2, 'Sản phẩm 8', 'Đây là một trong những nước hoa nổi tiếng nhất của Chanel với mùi hương hoa cỏ pha trộn cùng hương thơm đậm đà của vani và hổ phách.', 'Đây là một trong những nước hoa nổi tiếng nhất của Chanel với mùi hương hoa cỏ pha trộn cùng hương thơm đậm đà của vani và hổ phách.', 8000000, 18, 1, 10, 1, '2023-03-08 19:25:43', '2023-03-08 19:25:46'),
+	(9, 1, 2, 'Sản phẩm 9', 'Đây là một trong những nước hoa nổi tiếng nhất của Chanel với mùi hương hoa cỏ pha trộn cùng hương thơm đậm đà của vani và hổ phách.', 'Đây là một trong những nước hoa nổi tiếng nhất của Chanel với mùi hương hoa cỏ pha trộn cùng hương thơm đậm đà của vani và hổ phách.', 9000000, 18, 1, 10, 1, '2023-03-08 19:25:43', '2023-03-08 19:25:46');
+
+-- Dumping data for table ocean.product_cats: ~2 rows (approximately)
+INSERT INTO `product_cats` (`id`, `name`, `created_at`, `updated_at`) VALUES
+	(1, 'Nước hoa nam', '2023-03-08 19:19:09', '2023-03-08 19:19:10'),
+	(2, 'Nước hoa nữ', '2023-03-08 19:19:19', '2023-03-08 19:19:20');
+
+-- Dumping data for table ocean.product_comments: ~6 rows (approximately)
+INSERT INTO `product_comments` (`id`, `product_id`, `user_id`, `email`, `name`, `messages`, `rating`, `created_at`, `updated_at`) VALUES
+	(1, 1, 1, 'nhu123@gmail.com', 'nhu', 'tyt voi', 5, NULL, NULL),
+	(2, 1, 1, 'nhu123@gmail.com', 'nhu', 'tyt voi', 5, NULL, NULL),
+	(3, 1, 1, 'nhu123@gmail.com', 'nhu', 'tyt voi', 5, NULL, NULL),
+	(4, 1, 1, 'nhu123@gmail.com', 'nhu', 'tyt voi', 5, NULL, NULL),
+	(5, 1, 1, 'nhu123@gmail.com', 'nhu', 'tyt voi', 5, NULL, NULL),
+	(6, 1, 1, 'nhu123@gmail.com', 'nhu', 'tyt voi', 5, NULL, NULL);
+
+-- Dumping data for table ocean.product_details: ~6 rows (approximately)
+INSERT INTO `product_details` (`id`, `product_id`, `capacity`, `qty`, `created_at`, `updated_at`) VALUES
+	(1, 1, 10, 11, '2023-03-08 19:27:43', '2023-03-08 19:27:44'),
+	(2, 1, 10, 11, '2023-03-08 19:27:43', '2023-03-08 19:27:44'),
+	(3, 1, 10, 11, '2023-03-08 19:27:43', '2023-03-08 19:27:44'),
+	(4, 1, 10, 11, '2023-03-08 19:27:43', '2023-03-08 19:27:44'),
+	(5, 1, 10, 11, '2023-03-08 19:27:43', '2023-03-08 19:27:44'),
+	(6, 1, 10, 11, '2023-03-08 19:27:43', '2023-03-08 19:27:44');
+
+-- Dumping data for table ocean.product_images: ~9 rows (approximately)
+INSERT INTO `product_images` (`id`, `product_id`, `path`, `created_at`, `updated_at`) VALUES
+	(1, 1, 'nuochoa-1.jpg', '2023-03-08 19:30:00', '2023-03-08 19:30:05'),
+	(2, 2, 'nuochoa-2.jpg', '2023-03-08 19:30:01', '2023-03-08 19:30:07'),
+	(3, 3, 'nuochoa-3.jpg', '2023-03-08 19:30:01', '2023-03-08 19:30:07'),
+	(4, 4, 'nuochoa-4.jpg', '2023-03-08 19:30:02', '2023-03-08 19:30:08'),
+	(5, 5, 'nuochoa-5.jpg', '2023-03-08 19:30:02', '2023-03-08 19:30:09'),
+	(6, 6, 'nuochoa-6.jpg', '2023-03-08 19:30:03', '2023-03-08 19:30:09'),
+	(7, 7, 'nuochoa-7.jpg', '2023-03-08 19:30:03', '2023-03-08 19:30:10'),
+	(8, 8, 'nuochoa-8.jpg', '2023-03-08 19:30:04', '2023-03-08 19:30:10'),
+	(9, 9, 'nuochoa-9.jpg', '2023-03-08 19:30:05', '2023-03-08 19:30:11');
+
+-- Dumping data for table ocean.users: ~2 rows (approximately)
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+	(1, 'thuong', 'thuong150@gmail.com', '2023-03-08 19:12:29', 'dailinh11', NULL, NULL, NULL),
+	(2, 'linh', 'linh150@gmail.com', NULL, 'dailinh11', NULL, NULL, NULL),
+	(3, 'quynh', 'quynh123@gmail.com', NULL, 'quynh123', NULL, NULL, NULL);
+
+-- Dumping data for table ocean.volumes: ~6 rows (approximately)
+INSERT INTO `volumes` (`id`, `product_id`, `volume`, `created_at`, `updated_at`) VALUES
+	(1, 1, 10, '2023-03-12 07:07:31', '2023-03-12 07:07:35'),
+	(2, 1, 20, '2023-03-12 07:07:32', '2023-03-12 07:07:35'),
+	(3, 1, 50, '2023-03-12 07:07:33', '2023-03-12 07:07:36'),
+	(4, 1, 100, '2023-03-12 07:07:33', '2023-03-12 07:07:37'),
+	(5, 2, 50, '2023-03-12 07:07:48', '2023-03-12 07:07:49'),
+	(6, 2, 100, '2023-03-12 07:07:48', '2023-03-12 07:07:49');
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
